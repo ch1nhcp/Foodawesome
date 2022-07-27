26f5f10876e509c8505b55806d6bc41937c14f31
chinhcpdev <chinhcpdev@gmail.com>
2022-07-27 18:01:47 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
 import Search from "./components/Search";
-import { BrowserRouter } from "react-router-dom";
-
+import styled from "styled-components";
+import { BrowserRouter, Link } from "react-router-dom";
+import { ImSpoonKnife } from "react-icons/im";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <ImSpoonKnife />
+          <Logo to={"/"}>Foodawesome</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -14,5 +19,21 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 900;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
